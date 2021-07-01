9bb66decdac6fcabbd1716dfaf4d78cbbfc148ab
Yon <anaseto@bardinflor.perso.aquilenet.fr>
2021-07-01 13:13:07 +0000

    use a pointer for the game field, and finish FOV handling

--- actions.go
+++ actions.go
@@ -27,7 +27,7 @@ func (m *model) handleAction() gruid.Effect {
 	switch m.action.Type {
 	case ActionMovement:
 		np := m.game.ECS.Positions[m.game.ECS.PlayerID].Add(m.action.Delta)
-		m.MovePlayer(np)
+		m.game.MovePlayer(np)
 	case ActionQuit:
 		// for now, just terminate with gruid End command: this will
 		// have to be updated later when implementing saving.
@@ -36,37 +36,37 @@ func (m *model) handleAction() gruid.Effect {
 	return nil
 }
 
-// MovePlayer moves the player to a given position.
-func (m *model) MovePlayer(to gruid.Point) {
-	if !m.game.Map.Walkable(to) {
+// MovePlayer moves the player to a given position and updates FOV information.
+func (g *game) MovePlayer(to gruid.Point) {
+	if !g.Map.Walkable(to) {
 		return
 	}
 	// We move the player to the new destination.
-	m.game.ECS.MovePlayer(to)
+	g.ECS.MovePlayer(to)
 	// Update FOV.
-	m.UpdateFOV()
+	g.UpdateFOV()
 }
 
 // UpdateFOV updates the field of view.
-func (m *model) UpdateFOV() {
+func (g *game) UpdateFOV() {
+	player := g.ECS.Player()
 	// player position
-	player := m.game.ECS.Player()
-	pp := m.game.ECS.Positions[m.game.ECS.PlayerID]
+	pp := g.ECS.Positions[g.ECS.PlayerID]
 	// We shift the FOV's Range so that it will be centered on the new
 	// player's position.
 	rg := gruid.NewRange(-maxLOS, -maxLOS, maxLOS+1, maxLOS+1)
-	player.FOV.SetRange(rg.Add(pp).Intersect(m.game.Map.Grid.Range()))
+	player.FOV.SetRange(rg.Add(pp).Intersect(g.Map.Grid.Range()))
 	// We mark cells in field of view as explored. We use the symmetric
 	// shadow casting algorithm provided by the rl package.
 	passable := func(p gruid.Point) bool {
-		return m.game.Map.Grid.At(p) != Wall
+		return g.Map.Grid.At(p) != Wall
 	}
 	for _, p := range player.FOV.SSCVisionMap(pp, maxLOS, passable, false) {
 		if paths.DistanceManhattan(p, pp) > maxLOS {
 			continue
 		}
-		if !m.game.Map.Explored[p] {
-			m.game.Map.Explored[p] = true
+		if !g.Map.Explored[p] {
+			g.Map.Explored[p] = true
 		}
 	}
 }
